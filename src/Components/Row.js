b4e96087da9f6a13a034bf523e0e2d3624cdd8d5
devynqualls <devyn.qualls@gmail.com>
2023-05-31 22:39:18 -0400

    NETFLIX-CLONE: Push to deploy

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./row.css";
 import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const Row = ({ title, movies, isMainRow }) => {
+  const [trailerURL, setTrailerURL] = useState("")
+
   const baseUrl = "https://image.tmdb.org/t/p/original/";
+  
   const opts = {
     height: "400",
     width: "100%",
     playerVars: {
     /* "https://developers.google.com/youtube/player_parameters", */ autoplay: 1, }  
   }
+
+  const handleClick = (movie) => {
+    if (trailerURL) {
+      setTrailerURL('')
+    } else {
+      movieTrailer(movie?.name || "")
+      .then(url => {
+        // https://www.youtube.com/watch?v=
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTrailerURL(urlParams.get('v'))
+          console.log(URL)
+      })
+      .catch((error) => console.log(error))
+    }
+  }
+
   return (
     <div>
       <h2 className="row-title">{title}</h2>
       <div className="posters">
         {movies.map((movie) => (
           <img
+            onClick={() => handleClick(movie)}
             key={movie.id}
             className={`row__poster ${isMainRow && "row_poster_main"}`}
             src={`${baseUrl}${
@@ -26,7 +47,7 @@ const Row = ({ title, movies, isMainRow }) => {
           />
         ))}
       </div>
-      {/* {trailerURL && <YouTube videoId={trailerURL} opts={opts} />} */}
+      {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
     </div>
   );
 };
